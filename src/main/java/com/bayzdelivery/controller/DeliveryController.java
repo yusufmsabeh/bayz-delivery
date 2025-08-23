faa9997102aed81784e5556889784110b1ebefc2
Bayzat <support@bayzat.com>
2025-08-23 11:01:14 +0300

    fix: global path /delivery

--- src/main/java/com/bayzdelivery/controller/DeliveryController.java
+++ src/main/java/com/bayzdelivery/controller/DeliveryController.java
@@ -3,25 +3,22 @@ package com.bayzdelivery.controller;
 import com.bayzdelivery.model.Delivery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.bayzdelivery.service.DeliveryService;
 
 @RestController
+@RequestMapping("/delivery")
 public class DeliveryController {
 
   @Autowired
   DeliveryService deliveryService;
 
-  @PostMapping(path ="/delivery")
+  @PostMapping()
   public ResponseEntity<Delivery> createNewDelivery(@RequestBody Delivery delivery) {
     return ResponseEntity.ok(deliveryService.save(delivery));
   }
 
-  @GetMapping(path = "/delivery/{delivery-id}")
+  @GetMapping(path = "/{delivery-id}")
   public ResponseEntity<Delivery> getDeliveryById(@PathVariable(name="delivery-id",required=true)Long deliveryId){
     Delivery delivery = deliveryService.findById(deliveryId);
     if (delivery !=null)
