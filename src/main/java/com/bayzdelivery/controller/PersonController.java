6913dce89919a6f07eed5ebef5936dbaff4dfdf5
Bayzat <support@bayzat.com>
2025-08-23 11:02:18 +0300

    refactor: remove unnecessary parentheses

--- src/main/java/com/bayzdelivery/controller/PersonController.java
+++ src/main/java/com/bayzdelivery/controller/PersonController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.bayzdelivery.service.PersonService;
 
-@RestController()
+@RestController
 @RequestMapping("/person")
 public class PersonController {
 
